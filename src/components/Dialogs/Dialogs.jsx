7a79540b0f8871c859486edd478769fa5da8f52a
Demenev Denis <DemenevDeni5@yandex.ru>
2025-04-06 10:02:40 +0300

    Урок 21: Верстка компоненты Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,47 @@
 import React from "react";
 import c from './Dialogs.module.css'
 
+const activeFriend = () => {
+	return (
+		(active) => active.isActive ? c.active : c.dialog
+	)
+}
+
 const Dialogs = () => {
 	return (
-		<>
-			Dialogs
-		</>
+		<div className={c.dialogs}>
+			<div className={c.dialogsItem}>
+				<div className={c.dialog}>
+					Dmitriy
+				</div>
+				<div className={c.dialog}>
+					Andrey
+				</div>
+				<div className={c.dialog}>
+					Sveta
+				</div>
+				<div className={c.dialog}>
+					Sasha
+				</div>
+				<div className={c.dialog}>
+					Viktor
+				</div>
+				<div className={c.dialog}>
+					Valeriy
+				</div>
+			</div>
+			<div className={c.messages}>
+				<div className={c.message}>
+					Hi
+				</div>
+				<div className={c.message}>
+					Hello
+				</div>
+				<div className={c.message}>
+					Yo
+				</div>
+			</div>
+		</div>
 	)
 }
 
